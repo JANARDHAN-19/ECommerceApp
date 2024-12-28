aed8ca257dc3cb5cdafa0c12762a9443a803140d
ja204 <jagajanardhan1@gmail.com>
2024-12-28 19:01:20 +0530

    Add project files.

---
+++ Models/Payment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ECommerceApp.Models;
+
+public partial class Payment
+{
+    public int PaymentId { get; set; }
+
+    public int OrderId { get; set; }
+
+    public DateTime? PaymentDate { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public string PaymentMethod { get; set; } = null!;
+
+    public string? TransactionId { get; set; }
+
+    public string? Status { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+
+    public virtual Order Order { get; set; } = null!;
+    public decimal? Discount { get; internal set; }
+}
